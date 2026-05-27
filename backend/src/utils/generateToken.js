794ddf0312f6987ebcc45a85e6cfe8d343a2a132
pjesmica <148099528+pjesmica@users.noreply.github.com>
2026-05-27 20:16:03 +0200

    Update generateToken.js

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -12,7 +12,7 @@ const generateToken = (res, userId) => {
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
-    return token; // 🔥 OVO FALI
+    return token; 
 };
 
 export default generateToken;
